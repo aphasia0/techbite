b46f03c5d1c7783c4e0ab026a9bfd85970984519
Antonio Giordano <tony.dream@gmail.com>
2021-08-14 18:30:42 +0200

    ng zorro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [{ path: 'blog', loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule) }, { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/welcome' },
+  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
