dc57667bc57ce49ffe5c5ec2af0a950561a9c2fe
Antonio Giordano <tony.dream@gmail.com>
2021-08-18 11:28:09 +0200

    sss

--- scully.techbite.config.ts
+++ scully.techbite.config.ts
@@ -3,6 +3,7 @@ export const config: ScullyConfig = {
   projectRoot: "./src",
   projectName: "techbite",
   outDir: './dist/static',
+  puppeteerLaunchOptions: {executablePath: '/opt/build/repo/node_modules/chromium'},
   routes: {
     '/blog/:slug': {
       type: 'contentFolder',
