3c4f95a9d324863eb4934e13c5cb90bffe1bab33
Antonio Giordano <tony.dream@gmail.com>
2021-08-20 16:42:26 +0200

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
-import { ScullyRoute, ScullyRoutesService } from '@scullyio/ng-lib';
-import { Observable } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
+import {Component} from '@angular/core';
+import {NgForm} from '@angular/forms';
+import {Router} from '@angular/router';
+import {ScullyRoute, ScullyRoutesService} from '@scullyio/ng-lib';
+import {Observable} from 'rxjs';
+import {filter, map, tap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -9,20 +11,30 @@ import { filter, map } from 'rxjs/operators';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-
-  inputValue?: string;
-  options: string[] = [];
+  options: string[] | undefined = [] as any;
   isCollapsed = false;
 
-  links$: Observable<ScullyRoute[]> = this.scully.available$.pipe(
-    map((routes) => routes.filter((route) => route.route !== '/'))
-  );
+  links: ScullyRoute[] | undefined;
 
-  constructor(private scully: ScullyRoutesService) {}
-
+  constructor(private scully: ScullyRoutesService, private router: Router) {
+    this.scully.available$
+      .pipe(map((routes) => routes.filter((route) => route.route.startsWith('/blog/'))))
+      .subscribe((x) => (this.links = x));
+  }
 
   onInput(event: Event): void {
     const value = (event.target as HTMLInputElement).value;
-    this.options = value ? [value, value + value, value + value + value] : [];
+    this.options = this.links
+      ?.map((x) => (x.title ? x.title : ''))
+      .filter((x) => x.toUpperCase().includes(value.toUpperCase()));
+  }
+
+  search(f: NgForm) {
+    const route = this.links?.filter(x => x.title === f.value.search).map(x => x.route)
+    console.log(route)
+    if (route) {
+      this.router.navigate(route);
+    }
+
   }
 }
