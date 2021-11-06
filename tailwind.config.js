362ab7d25d339d675c1cddf10c2913f5059b1cee
Antonio Giordano <tony.dream@gmail.com>
2021-11-06 14:29:23 +0100

    build

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+const { guessProductionMode } = require("@ngneat/tailwind");
+
+process.env.TAILWIND_MODE = guessProductionMode() ? 'build' : 'watch';
+
+module.exports = {
+    prefix: '',
+    purge: {
+      content: [
+        './src/**/*.{html,ts,css,scss,sass,less,styl}',
+      ]
+    },
+    darkMode: 'class', // or 'media' or 'class'
+    theme: {
+      backgroundColor: theme => ({
+        ...theme('colors'),
+        'primary': '#053017',
+        'secondary': '#ffed4a',
+        'danger': '#e3342f',
+      })
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [require('@tailwindcss/typography')],
+};
+
+
+
