c23505b30303affa5eac2480936429831c66fc91
Antonio Giordano <antonio.giordano@cagroupsolutions.it>
2024-10-10 21:15:22 +0200

    theming

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,29 @@ import {Component} from '@angular/core';
 import {NgForm} from '@angular/forms';
 import {blogPages, Page} from "./static.pages";
 import {Router} from "@angular/router";
+import { PrimeNGConfig } from 'primeng/api';
+import { definePreset } from 'primeng/themes';
+import { Aura } from 'primeng/themes/aura';
+
+
+const MyPreset = definePreset(Aura, {
+  semantic: {
+      primary: {
+          50: '{indigo.50}',
+          100: '{indigo.100}',
+          200: '{indigo.200}',
+          300: '{indigo.300}',
+          400: '{indigo.400}',
+          500: '{indigo.500}',
+          600: '{indigo.600}',
+          700: '{indigo.700}',
+          800: '{indigo.800}',
+          900: '{indigo.900}',
+          950: '{indigo.950}'
+      }
+  }
+});
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -13,9 +36,15 @@ export class AppComponent {
   isDark: any;
   links: Page[] = blogPages;
   model:any;
+  
 
-  constructor(private router: Router) {
+  constructor(private router: Router,private config: PrimeNGConfig) {
+    this.config.theme.set({ preset: Aura ,
+            options: {
+                darkModeSelector: '.dark',
+            }});
   }
+
   onInput(event: Event): void {
 
     const value = (event.target as HTMLInputElement).value;
