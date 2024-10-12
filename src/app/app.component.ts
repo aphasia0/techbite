caae457402a301381e5ed65439455c93453aca4a
Antonio Giordano <antonio.giordano@cagroupsolutions.it>
2024-10-12 14:34:14 +0200

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,26 +4,8 @@ import {blogPages, Page} from "./static.pages";
 import {Router} from "@angular/router";
 import { PrimeNGConfig } from 'primeng/api';
 import { definePreset } from 'primeng/themes';
-import { Aura } from 'primeng/themes/aura';
+import { Nora } from 'primeng/themes/nora';
 
-
-const MyPreset = definePreset(Aura, {
-  semantic: {
-      primary: {
-          50: '{indigo.50}',
-          100: '{indigo.100}',
-          200: '{indigo.200}',
-          300: '{indigo.300}',
-          400: '{indigo.400}',
-          500: '{indigo.500}',
-          600: '{indigo.600}',
-          700: '{indigo.700}',
-          800: '{indigo.800}',
-          900: '{indigo.900}',
-          950: '{indigo.950}'
-      }
-  }
-});
 
 @Component({
   selector: 'app-root',
@@ -39,9 +21,13 @@ export class AppComponent {
   
 
   constructor(private router: Router,private config: PrimeNGConfig) {
-    this.config.theme.set({ preset: Aura ,
+    this.config.theme.set({ preset: Nora ,
             options: {
                 darkModeSelector: '.dark',
+                cssLayer: {
+                  name: 'primeng',
+                  order: 'tailwind-base, primeng, tailwind-utilities'
+              }
             }});
   }
 
