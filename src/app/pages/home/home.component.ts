a4ef5ca12324379d07c61507dbfc92e00a213aa8
Antonio Giordano <antonio.giordano@cagroupsolutions.it>
2024-12-08 09:30:48 +0100

    small fix

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -5,10 +5,5 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
-
-
-  ngOnInit() {
-
-  }
+export class HomeComponent{
 }
