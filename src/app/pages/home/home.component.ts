ad978768c520cf94ee49c7e61bdf4714ec38d3f3
Antonio Giordano <antonio.giordano@cagroupsolutions.it>
2026-02-08 14:54:00 +0100

    feat: Introduce a "Latest Posts" section on the home page and update sidebar blog post title styling.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { blogPages, Page } from '../../static.pages';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent{
+export class HomeComponent {
+  latestPosts: Page[];
+
+  constructor() {
+    // Sort by date descending and take the first 3
+    this.latestPosts = [...blogPages]
+      .sort((a, b) => b.date.localeCompare(a.date))
+      .slice(0, 3);
+  }
 }
